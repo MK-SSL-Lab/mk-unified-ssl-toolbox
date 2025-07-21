aad43cbb3f14d05a0cd024d048fbd7b0c94b21c5
emika s <mel.shirian@gmail.com>
2025-07-21 14:57:58 +0330

    audioclip debug

--- MK_SSL/multimodal/models/modules/backbones.py
+++ MK_SSL/multimodal/models/modules/backbones.py
@@ -263,6 +263,7 @@ class FBSPFrontEnd(nn.Module):
         x = x.unsqueeze(1)  # [B, 1, L]
         x = self.relu(self.bn1(self.conv1(x)))  # [B, n_filters, T]
         x = self.relu(self.bn2(self.conv2(x)))  # [B, n_filters, T]
+        print(f"[DEBUG] AudioCLIP.forward: audio_input.shape = {x.shape}")
         return x  
 
 
