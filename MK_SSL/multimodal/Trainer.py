d063b0fb6feb2955e55edba74d59839ff80ada1d
emika s <mel.shirian@gmail.com>
2025-07-23 01:34:02 +0330

    clap debug

--- MK_SSL/multimodal/Trainer.py
+++ MK_SSL/multimodal/Trainer.py
@@ -969,11 +969,13 @@ class Trainer:
         if self.reload_checkpoint:
             start_epoch = self._reload_latest_checkpoint() + 1 # +1 because _reload returns 0-indexed epoch
 
+        collator = AudioTextCollator()
+
         train_loader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=batch_size,
             shuffle=True,
-             collate_fn=collator,
+            collate_fn=collator,
             num_workers=self.num_workers,
         )
         first_train_batch = next(iter(train_loader))
