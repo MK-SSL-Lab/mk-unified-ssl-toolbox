4ed5544ebfc7ee939f4892bc37d8bc6b5ef50e73
Kianoosh <kia.vadaei@gmail.com>
2025-08-05 01:47:46 +0330

    removing clamp from CLAP

--- MK_SSL/multimodal/models/modules/heads.py
+++ MK_SSL/multimodal/models/modules/heads.py
@@ -3,22 +3,18 @@ import torch.nn as nn
 from typing import List, Tuple, Optional
 
 
-class ProjectionHead(nn.Module):
+class ProjectionHead(nn.Sequential):
     """
-    Base class for all projection and prediction heads.
+    Projection MLP head for transforming encoder outputs.
 
     Args:
-        blocks:
-            List of tuples, each denoting one block of the projection head MLP.
-            Each tuple reads (in_features, out_features, batch_norm_layer,
-            non_linearity_layer).
+        blocks: List of tuples, each defining one layer of the MLP.
+                Each tuple is (in_features, out_features, batch_norm_layer, activation_layer).
     """
 
     def __init__(
         self, blocks: List[Tuple[int, int, Optional[nn.Module], Optional[nn.Module]]]
     ):
-        super().__init__()
-
         layers = []
         for input_dim, output_dim, batch_norm, non_linearity in blocks:
             use_bias = not bool(batch_norm)
@@ -27,10 +23,8 @@ class ProjectionHead(nn.Module):
                 layers.append(batch_norm)
             if non_linearity:
                 layers.append(non_linearity)
-        self.layers = nn.Sequential(*layers)
+        super().__init__(*layers)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layers(x)
 
 
 class Wav2ClipProjectionHead(ProjectionHead):
